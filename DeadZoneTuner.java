2faa3e92538d08fc61d0ffb52df3f274ebabd84a
AdmiralTryhard <nikolai.solo.b@gmail.com>
2019-08-16 09:10:33 -0500

    renamed, and readjusted values, and added some more documentation

--- DeadZoneTuner.java
+++ DeadZoneTuner.java
@@ -11,15 +11,15 @@ public class DeadZoneTuner{
  /**
  * adjustForDeadzone is entirely meant for the deadzone to be used, and it automatically utilizes a previously derived math equation
  * to start
- * @param input
+ * @param input this number is typically a controller value, regardless of axis.
  * @param minimumrange
  * @param maximumrange
- * @param deadzone
+ * @param deadzone how big is it?
  * @param isInverted just to quickly
  * 
  */
 
-    public double adjustForDeadzone(double tobescaled, double deadzone, boolean isInverted){
+    public double adjustForDeadzone(double input, double deadzone, boolean isInverted){
         double result;
         if(Math.abs(input) <= deadzone){
             result = 0;
@@ -36,7 +36,7 @@ public class DeadZoneTuner{
     }
     /** 1st and foremost, this assumes that the minimum input is the opposite of maximum, otherwise initial scale for your input (as it is the most common for our applications)
      * This will let you adjust the input to any scale that you want by setting the minimum and maximum.
-     * @param input this is the number that you will be adjusting to scale
+     * @param input this is the number that you will be adjusting to scale (usually on a scale of its own)
      * @param minimuminput this is the smallest number your input can give
      * @param maximuminput this is the biggest number your input can give
      * @param minimumdesired this will be the smallest number that the function will input that you want if you bring your input to its minimum
@@ -48,7 +48,6 @@ public class DeadZoneTuner{
 
         double initialscale = (input + maximuminput) / (maximuminput - minimuminput);
 
-       
         output = (initialscale * (maximumdesired - minimumdesired) + minimumdesired);
         
         return output;
