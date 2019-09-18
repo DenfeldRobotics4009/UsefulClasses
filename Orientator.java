482bcdb8d2f53653a0afcee4a4586a51b10ff11b
AdmiralTryhard <nikolai.solo.b@gmail.com>
2019-09-18 17:51:55 -0500

    added a special case of angles greater than 180 degrees

--- Orientator.java
+++ Orientator.java
@@ -5,33 +5,48 @@ import java.lang.Math;
  */
 public class Orientator {
 
-    public double desiredangle, currentangle, desiredmagnitude;
+    public double desiredangle, currentangle, relativeAngle, desiredmagnitude, xOutput, yOutput;
 
     /**
-     * You need to know where you're facing.
-     * @param inputAngle the thing that you want to set as your angle
+     * You need to know where you're currently facing.
+     * @param inputAngle the thing that you want to set as your angle. 
+     * You must have an angle from 0 to 360 degrees (not including 360).
      */
 
     public void setCurrentAngle(double inputAngle) {
         inputAngle = currentangle;
     }
     /**
-     * Finds the relative angle for the object to travel towards.
+     * Finds the relative angle for the object to travel towards, so that your motion matches the angle you want.
      * @param x the horizontal input that you give (so, the strafe part)
      * @param y the forward/backwards input that you have.
      * 
      */
-    public double findDesiredAngle(double x, double y){
+    public double getRelativeAngle(double x, double y){
+        if(y >= 0){
 
         desiredangle = Math.atan(x/y);
+        }
         
-        double relativeAngle = -1*(currentangle + desiredangle);
+        else {
+            desiredangle = Math.atan(x/y) + 180;
+        }
         
+        relativeAngle = -1*(currentangle + desiredangle);
+
+        if(relativeAngle < 0){
+            return relativeAngle + 360;
+        }
+        else {
         return relativeAngle;
+        }
     }
 
-    public double convertAngle(){
-        return -1 * (desiredangle + currentangle);
+
+    public double getOutputComponents(double magnitude){
+
+        xOutput = magnitude * Math.cos(desiredangle);
+        yOutput = magnitude * Math.sin(desiredangle); 
     }
 
 
